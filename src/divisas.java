2e48d05a5653646ec35221cf6b027c26625c0d70
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-07-29 21:07:52 -0500

    Subir archivos y clases
    Se procedió a subir las clases divisas y convertir, adicional se carga el formulario creado.

---
+++ src/divisas.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class divisas extends JFrame {
+    private JTextField divisa;
+    private JTextField divisafinal;
+    private JComboBox combo2;
+    private JComboBox combo1;
+    private JButton convertir;
+    private JPanel mainPanel;
+    private JButton limpiar;
+
+    public divisas() {
+
+    }
+}
